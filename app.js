0c71328151e2c0a01fbf99b51f1f576fadf947e6
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-04-07 03:28:23 +0300

    Update app.js
    finished

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 "use strict";
 
 const myInput = document.getElementById("name");
-
 const dollar = document.getElementById("dollar");
 const euro = document.getElementById("euro");
 const lira = document.getElementById("lira");
 const ruble = document.getElementById("ruble");
 const grivna = document.getElementById("grivna");
+
+const getDataOnload = () => {
+  fetch(
+    `http://api.currencylayer.com/live?access_key=d3174dd674f6831ebfa3ab45b697537c&currencies=EUR,USD,TRY,RUB,UAH&source=USD`
+  )
+    .then((res) => res.json())
+    .then(displayCurrency);
+  myInput.value = 1;
+};
 
 const getData = () => {
   fetch(
@@ -18,9 +26,9 @@ const getData = () => {
 myInput.addEventListener("input", getData);
 
 const displayCurrency = (res) => {
-  euro.innerText = `${res.quotes.USDEUR}` * myInput.value;
-  lira.innerText = `${res.quotes.USDTRY}` * myInput.value;
-  ruble.innerText = `${res.quotes.USDRUB}` * myInput.value;
-  grivna.innerText = `${res.quotes.USDUAH}` * myInput.value;
+  euro.innerText = (`${res.quotes.USDEUR}` * myInput.value).toFixed(2);
+  lira.innerText = (`${res.quotes.USDTRY}` * myInput.value).toFixed(2);
+  ruble.innerText = (`${res.quotes.USDRUB}` * myInput.value).toFixed(2);
+  grivna.innerText = (`${res.quotes.USDUAH}` * myInput.value).toFixed(2);
 };
-
+window.onload = getDataOnload();

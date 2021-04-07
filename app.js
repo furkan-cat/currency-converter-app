4045aca99f64380ae72131ac1f77e0cf61f161f7
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-04-07 03:48:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ const getDataOnload = () => {
     `http://api.currencylayer.com/live?access_key=d3174dd674f6831ebfa3ab45b697537c&currencies=EUR,USD,TRY,RUB,UAH&source=USD`
   )
     .then((res) => res.json())
-    .then(displayCurrency);
+    .then(displayCurrencyOnload);
   myInput.value = 1;
 };
+
+const displayCurrencyOnload = (res) => {
+  euro.innerText = (`${res.quotes.USDEUR}` * myInput.value).toFixed(2);
+  lira.innerText = (`${res.quotes.USDTRY}` * myInput.value).toFixed(2);
+  ruble.innerText = (`${res.quotes.USDRUB}` * myInput.value).toFixed(2);
+  grivna.innerText = (`${res.quotes.USDUAH}` * myInput.value).toFixed(2);
+};
+
+window.onload = getDataOnload();
 
 const getData = () => {
   fetch(
@@ -31,4 +40,3 @@ const displayCurrency = (res) => {
   ruble.innerText = (`${res.quotes.USDRUB}` * myInput.value).toFixed(2);
   grivna.innerText = (`${res.quotes.USDUAH}` * myInput.value).toFixed(2);
 };
-window.onload = getDataOnload();
